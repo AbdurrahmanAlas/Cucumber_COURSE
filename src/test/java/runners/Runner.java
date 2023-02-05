cfdcc330e53af117d5a644636e375056f5bf725a
Abdurrahman Alas <63901054+AbdurrahmanAlas@users.noreply.github.com>
2023-02-05 18:02:44 +0300

    automation

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resource/features" ,
         glue = "stepdefinitions" ,
         tags= "@wip",
-        dryRun = false
+        dryRun = true
 )
 
 public class Runner {
